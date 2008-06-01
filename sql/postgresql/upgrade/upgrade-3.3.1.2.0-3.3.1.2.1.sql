21d71c635e459c6c7d51c342fcd5e0545fce14af
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-06-01 18:08:23 +0000

    - added report_description

--- sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
+++ sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
@@ -37,3 +37,20 @@ select inline_0 ();
 drop function inline_0();
 
 
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count			integer;
+begin
+	select  count(*) into v_count from user_tab_columns
+	where   lower(table_name) = ''im_reports'' and lower(column_name) = ''report_description'';
+	IF v_count > 0 THEN return 0; END IF;
+	
+	alter table im_reports add report_description text;
+	
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0();
+
+
