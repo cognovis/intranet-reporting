257adf9a300d0a515e1496eb903dbdb091759ae7
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-02-23 14:51:09 +0000

    -- fixed log

--- sql/postgresql/upgrade/upgrade-3.4.1.0.2-3.4.1.0.3.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.2-3.4.1.0.3.sql
@@ -1,6 +1,6 @@
 -- upgrade-3.4.1.0.2-3.4.1.0.3.sql
 
--- SELECT acs_log__debug('/packages/intranet-reporting/sql/postgresql/upgrade/upgrade-3.4.1.0.2-3.4.1.0.3.sql','');
+SELECT acs_log__debug('/packages/intranet-reporting/sql/postgresql/upgrade/upgrade-3.4.1.0.2-3.4.1.0.3.sql','');
 
 -- deprecated 
 
